f79eea2934544ba21be8111f8e96567a9f46e37c
Ayush Barman <barmanayush2980@gmail.com>
2025-02-09 12:06:22 +0530

    Basic setup

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import HeroSection from "@/components/heroSection.component";
+import { Fragment } from "react"
+
+const LandingPage = () => {
+    return <Fragment>
+        <HeroSection/>
+    </Fragment>
+}
+
+export default LandingPage;
---
+++ components/heroSection.component.tsx
@@ -0,0 +1,9 @@
+import { Fragment } from 'react'
+const HeroSection = () => {
+    return <Fragment>
+        <div className="container" >
+            Hero section
+        </div>
+    </Fragment>
+}
+export default HeroSection;
